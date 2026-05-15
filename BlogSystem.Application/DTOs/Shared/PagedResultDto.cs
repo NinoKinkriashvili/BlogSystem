457c3265dc57d12a3bf068aa358d4edef8f97bf1
Nino Kinkriashvili <ninokinkriashvili13@gmail.com>
2026-05-15 18:33:20 +0400

    PH-1.4 - Application Layer - Shared DTOs

---
+++ BlogSystem.Application/DTOs/Shared/PagedResultDto.cs
@@ -0,0 +1,9 @@
+namespace BlogSystem.Application.DTOs.Shared;
+
+public class PagedResultDto<T>
+{
+    public IEnumerable<T> Items { get; set; } = new List<T>();
+    public int TotalCount { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+}
