de1af34459917eab4bdbe12ef03b64ed94e4f4fb
Nino Kinkriashvili <ninokinkriashvili13@gmail.com>
2026-05-17 19:08:25 +0400

    PH-1.11 - Application Layer - DI

--- BlogSystem.Application/DependencyInjection.cs
+++ BlogSystem.Application/DependencyInjection.cs
@@ -1,6 +1,26 @@
+using BlogSystem.Application.Interfaces.Services;
+using BlogSystem.Application.Services;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
 namespace BlogSystem.Application;
 
-public class DependencyInjection
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        // AutoMapper
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        // FluentValidation
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        // Services
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IPostService, PostService>();
+        services.AddScoped<IAuthService, AuthService>();
+
+        return services;
+    }
 }
