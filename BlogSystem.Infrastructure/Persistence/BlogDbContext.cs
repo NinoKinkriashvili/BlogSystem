c2b6277701a0e24f3f91ab9e14b76097d04cbdbd
Nino Kinkriashvili <ninokinkriashvili13@gmail.com>
2026-05-17 20:21:20 +0400

    PH-2.1 - Infrastructure Layer - DbContext

---
+++ BlogSystem.Infrastructure/Persistence/BlogDbContext.cs
@@ -0,0 +1,22 @@
+using BlogSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogSystem.Infrastructure.Persistence;
+
+public class BlogDbContext : DbContext
+{
+    public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(BlogDbContext).Assembly);
+    }
+}
