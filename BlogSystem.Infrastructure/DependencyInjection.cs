485f72cc715dc6c52b8a42e2cf9f88a4aa2cd1e4
Nino Kinkriashvili <ninokinkriashvili13@gmail.com>
2026-05-18 15:16:43 +0400

    PH-2.6 - Infrastructure Layer - DI

---
+++ BlogSystem.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,30 @@
+using BlogSystem.Application.Interfaces.Repositories;
+using BlogSystem.Infrastructure.Persistence;
+using BlogSystem.Infrastructure.Persistence.Repositories;
+using BlogSystem.Infrastructure.Security;
+using BlogSystem.Infrastructure.Seed;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlogSystem.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<BlogDbContext>(options =>
+            options.UseSqlServer(
+                configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IPostRepository, PostRepository>();
+
+        services.AddSingleton<PasswordHasher>();
+        services.AddScoped<JwtService>();
+
+        return services;
+    }
+}
