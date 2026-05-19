846d4c3e5ac5cdf2d6c7063cab0e17df036b6733
Nino Kinkriashvili <ninokinkriashvili13@gmail.com>
2026-05-19 21:42:21 +0400

    PH-3.4 - WEB Layer - Program Class

--- BlogSystem.Web/Program.cs
+++ BlogSystem.Web/Program.cs
@@ -1,27 +1,59 @@
+using BlogSystem.Application;
+using BlogSystem.Infrastructure;
+using BlogSystem.Application.Validators.User;
+using FluentValidation;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// MVC
 builder.Services.AddControllersWithViews();
+
+// Layers
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+// AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// FluentValidation
+builder.Services.AddValidatorsFromAssemblyContaining<RegisterUserDtoValidator>();
+
+// Authentication / Authorization (Cookie for now)
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/auth/login";
+        options.LogoutPath = "/auth/logout";
+        options.AccessDeniedPath = "/auth/login";
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Error handling
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+// Pipeline
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
+// MVC routes
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// API controllers
+app.MapControllers();
 
 app.Run();
