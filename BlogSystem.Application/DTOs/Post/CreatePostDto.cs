3f4de0cc4fd1195da91f22472b6d19317bd1dc01
Nino Kinkriashvili <ninokinkriashvili13@gmail.com>
2026-05-21 23:40:54 +0400

    PH-1.4 - Application Layer - Update: Post DTOs

--- BlogSystem.Application/DTOs/Post/CreatePostDto.cs
+++ BlogSystem.Application/DTOs/Post/CreatePostDto.cs
@@ -4,5 +4,4 @@ public class CreatePostDto
 {
     public string Title { get; set; } = string.Empty;
     public string Content { get; set; } = string.Empty;
-    public Guid AuthorId { get; set; }
 }
